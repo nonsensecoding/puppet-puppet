0e575f816d18b48ee413adb9f77fc7d629948360
Maximilian Mayer <maximilian.mayer@sixt.de>
2015-05-21 17:10:17 +0200

    re-structured and renamed variables

--- manifests/agent.pp
+++ manifests/agent.pp
@@ -13,40 +13,33 @@
 class puppet::agent (
   $agent_config_file            = '/etc/puppetlabs/puppet/puppet.conf',
   $agent_config_content         = 'puppet/puppet.conf.epp',
-  $agent_symlink_puppet         = '/usr/local/bin/puppet',
-  $agent_symlink_puppet_target  = '/opt/puppetlabs/bin/puppet',
-  $agent_symlink_facter         = '/usr/local/bin/facter',
-  $agent_symlink_facter_target  = '/opt/puppetlabs/bin/facter',
-  $agent_symlink_cfacter        = '/usr/local/bin/cfacter',
-  $agent_symlink_cfacter_target = '/opt/puppetlabs/bin/cfacter',
-  $agent_symlink_hiera          = '/usr/local/bin/hiera',
-  $agent_symlink_hiera_target   = '/opt/puppetlabs/bin/hiera',
-  $agent_symlink_mco            = '/usr/local/bin/mco',
-  $agent_symlink_mco_target     = '/opt/puppetlabs/bin/mco',
   $agent_package_name           = 'puppet-agent',
   $agent_package_ensure         = 'installed',
   $agent_cron_name              = 'cron-puppetagent',
   $agent_cron_user              = 'root',
-  $agent_cron_minute            = 'UNSET',
+  $agent_cron_minute            = [fqdn_rand(15), fqdn_rand(15)+30],
   $agent_cron_day               = '*',
   $agent_cron_hour              = '*',
-  $agent_cron_command           = '/usr/local/bin/puppet agent -o --no-daemonize',
-  $report                       = true,
-  $classfile                    = '$vardir/classes.txt'
-  $graph                        = false,
-  $pluginsync                   = true,
+  $agent_cron_command           = '/usr/local/bin/puppet agent --o --no-daemonize',
+  $symlink_puppet         	= '/usr/local/bin/puppet',
+  $symlink_puppet_target  	= '/opt/puppetlabs/bin/puppet',
+  $symlink_facter         	= '/usr/local/bin/facter',
+  $symlink_facter_target  	= '/opt/puppetlabs/bin/facter',
+  $symlink_cfacter        	= '/usr/local/bin/cfacter',
+  $symlink_cfacter_target 	= '/opt/puppetlabs/bin/cfacter',
+  $symlink_hiera          	= '/usr/local/bin/hiera',
+  $symlink_hiera_target   	= '/opt/puppetlabs/bin/hiera',
+  $symlink_mco            	= '/usr/local/bin/mco',
+  $symlink_mco_target     	= '/opt/puppetlabs/bin/mco',
+  $report                       = 'true',
+  $classfile                    = '$vardir/classes.txt',
+  $graph                        = 'false',
+  $pluginsync                   = 'true',
   $waitforcert                  = '120',
 )
 {
 
   include cron
-
-  if $agent_cron_minute == 'UNSET' {
-    $cron_minute = [fqdn_rand(15), fqdn_rand(15)+30 ]
-  }
-  else {
-    $cron_minute = $agent_cron_minute
-  }
 
   package { $agent_package_name:
     ensure => $agent_package_ensure,
@@ -72,38 +65,38 @@ class puppet::agent (
     require  => [ Package[$agent_package_name], Service['crond'] ],
   }
 
-  file { 'agent_symlink_puppet':
+  file { 'symlink_puppet':
     ensure  => link,
-    path    => $agent_symlink_puppet,
-    target  => $agent_symlink_puppet_target,
+    path    => $symlink_puppet,
+    target  => $symlink_puppet_target,
     require => Package[$agent_package_name],
   }
 
-  file { 'agent_symlink_hiera':
+  file { 'symlink_hiera':
     ensure  => link,
-    path    => $agent_symlink_hiera,
-    target  => $agent_symlink_hiera_target,
+    path    => $symlink_hiera,
+    target  => $symlink_hiera_target,
     require => Package[$agent_package_name],
   }
 
-  file {'agent_symlink_facter':
+  file {'symlink_facter':
     ensure  => link,
-    path    => $agent_symlink_facter,
-    target  => $agent_symlink_facter_target,
+    path    => $symlink_facter,
+    target  => $symlink_facter_target,
     require => Package[$agent_package_name],
   }
 
-  file { 'agent_symlink_cfacter':
+  file { 'symlink_cfacter':
     ensure  => link,
-    path    => $agent_symlink_cfacter,
-    target  => $agent_symlink_cfacter_target,
+    path    => $symlink_cfacter,
+    target  => $symlink_cfacter_target,
     require => Package[$agent_package_name],
   }
 
-  file { 'agent_symlink_mco':
+  file { 'symlink_mco':
     ensure  => link,
-    path    => $agent_symlink_mco,
-    target  => $agent_symlink_mco_target,
+    path    => $symlink_mco,
+    target  => $symlink_mco_target,
     require => Package[$agent_package_name],
   }
 }
